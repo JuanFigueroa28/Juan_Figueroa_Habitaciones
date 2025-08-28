cb3cd428930d6fe879d746ea065004ead420c5cc
Juan Diego Figueroa Cárdenas <juanfigueroa1122475@correo.itm.edu.co>
2025-08-28 07:56:21 -0500

    Primer Commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Habitaciones habitacion1 = new Habitaciones();
+Habitaciones habitacion2 = new Habitaciones();
+public class Tipos
+{
+    public int Id = 0;
+    public String? TipoHabitacion = "";
+
+    public List<Habitaciones> Habitaciones = new List<Habitaciones>();
+}
+
+public class Elementos
+{
+    public String? seccion = "";
+    public int Id = 0;
+    public String? paredColor = "";
+    public int Televisores = 0;
+    public int banos = 0;
+
+}
+
+public class Habitaciones
+{
+    public int Id = 0;
+    public int NumHabitacion = 0;
+    public bool Activo = false;
+    public int valorNoche = 0;
+    public Tipos Tipo = new Tipos();
+    public List<Elementos> Elementos = new List<Elementos>();
+}
+
+public class Restricciones
+{
+    public int Id = 0;
+    public bool ruido = false;
+    public int numPersonas = 0;
+    public bool animales = true;
+
+}
